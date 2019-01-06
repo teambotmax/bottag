7d1ad32c9047d744b86de7a703da9be81826968a
TeamNoobBot <33267737+TeamNoobBot@users.noreply.github.com>
2019-01-06 20:35:25 +0800

    Add files via upload

---
+++ timeago/locales/en.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2016-5-26
+
+@author: hustcc
+'''
+
+LOCALE = [
+    ["just now", "just now"],
+    ["%s seconds ago", "%s seconds ago"],
+    ["1 minute ago", "1 minute ago"],
+    ["%s minutes ago", "%s minutes ago"],
+    ["1 hour ago", "1 hour ago"],
+    ["%s hours ago", "%s hours ago"],
+    ["1 day ago", "1 day ago"],
+    ["%s days ago", "%s days ago"],
+    ["1 week ago", "1 week ago"],
+    ["%s weeks ago", "%s weeks ago"],
+    ["1 month ago", "1 month ago"],
+    ["%s months ago", "%s months ago"],
+    ["1 year ago", "1 year ago"],
+    ["%s years ago", "%s years ago"],
+]
